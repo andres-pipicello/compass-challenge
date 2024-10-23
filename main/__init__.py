bc5efd757c132d3586cbd9a7a66f973291ce7c0b
andres-pipicello <andrespipicello@gmail.com>
2024-10-22 22:25:57 -0300

    Cleaned up main script so that output.csv gets generated

--- main/__init__.py
+++ main/__init__.py
@@ -1,8 +1,27 @@
 import csv
-from collections import Counter
 
 from matching import first_name_matching, Record, Comparison, last_name_matching, email_matching, zip_matching, \
     address_matching
+
+COMPARISON_SCORES = {
+    Comparison.MATCH: 1,
+    Comparison.LIKELY: 0.5,
+    Comparison.INCONCLUSIVE: 0.0,
+    Comparison.NO_MATCH: -0.5
+}
+
+# Completely arbitrary, but can be interpreted as "if the majority of the results are LIKELY o better", it is a match
+MATCH_THRESHOLD = 0.25
+
+
+def accuracy_for_score(score: float) -> str:
+    if score < MATCH_THRESHOLD:
+        return "Irrelevant"
+    if MATCH_THRESHOLD <= score < 0.5:
+        return "Low"
+    if 0.5 <= score < 0.8:
+        return "Medium"
+    return "High"
 
 
 def main():
@@ -14,14 +33,18 @@ def main():
         address_matching
     ]
 
-    with open('input.csv', mode='r') as file:
+    with open('input.csv', mode='rt') as file:
         reader = csv.DictReader(file)
         records = [Record(**row) for row in reader]
+
+    with open('output.csv', mode='w', newline='') as file:
+        writer = csv.writer(file, delimiter=',')
+
         for i in range(len(records)):
             for j in range(i + 1, len(records)):
                 (first, second) = (records[i], records[j])
-                comparison_results = {function: function(first, second) for function in comparison_functions}
-                comparison_counters = Counter(comparison_results.values())
-                if comparison_counters[Comparison.NO_MATCH] == 0:
-                    nice_results = sorted((key.name, count) for key, count in comparison_counters.items())
-                    print(f"Result: {nice_results} -> {first} and {second} match")
+                comparison_results = [function(first, second) for function in comparison_functions]
+                comparison_score = sum(COMPARISON_SCORES[result] for result in comparison_results) / len(comparison_functions)
+
+                if comparison_score >= MATCH_THRESHOLD:
+                    writer.writerow([first.contactID, second.contactID, accuracy_for_score(comparison_score)])
